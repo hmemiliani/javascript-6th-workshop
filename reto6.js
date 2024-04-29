fff2fbef168d78cd83df767cd024f22727bc9796
hmemiliani <harold_medranoe@hotmail.com>
2024-04-29 15:18:49 -0500

    Update reto-6

--- reto6.js
+++ reto6.js
@@ -9,16 +9,18 @@ setTimeout(() => {
 }, 1000);
 
 
+
+
+//¿Por qué "Mensaje 2: Con timeout de 0 segundos" no se muestra inmediatamente después de "Mensaje 1: Inmediatamente", a pesar de tener un retardo de 0 segundos?
 /*
-
-¿Por qué "Mensaje 2: Con timeout de 0 segundos" no se muestra inmediatamente después de "Mensaje 1: Inmediatamente", a pesar de tener un retardo de 0 segundos?
 R./
-Esto se debe a que JavaScript maneja los tiempos de manera asíncrona y delega la ejecución de tareas con temporizadores a las Web APIs del navegador. Aunque el retardo de setTimeout es de 0 segundos, JavaScript coloca esta tarea en la cola de tareas y la ejecuta después de que todas las tareas síncronas se completen. Por lo tanto, "Mensaje 2: Con timeout de 0 segundos" se muestra después de que JavaScript haya completado la ejecución del código síncrono.
+Javascript controla los tiempos de manera asincrona, pone las tareas con temporizadores a las web APIs del navegador, aunque el temporizador (setTimeout) sea cero(0) Javascript pone la tarea en la lista de tareas y las ejecuta despues de que las sincronas terminen su ejecucion. Por lo tanto el mensaje 2 solo se ejecuta cuando se completen los sincronos.
+*/
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-¿Qué nos dicen este comportamiento sobre el event loop, las macro y micro tareas, y la forma en que JavaScript maneja las operaciones asíncronas?
+//¿Qué nos dicen este comportamiento sobre el event loop, las macro y micro tareas, y la forma en que JavaScript maneja las operaciones asíncronas?
+/*
 R./
-Este comportamiento ilustra cómo funciona el event loop de JavaScript y cómo maneja las tareas asíncronas. Cuando se ejecuta el código, las tareas síncronas se ejecutan de inmediato, mientras que las tareas asíncronas, como las funciones de temporizador de setTimeout, se colocan en la cola de tareas y se ejecutan después de que todas las tareas síncronas se completen. Esto muestra cómo JavaScript puede manejar operaciones asíncronas de manera eficiente sin bloquear la ejecución del código.
-
-
+Cuando se ejecuta el codigo, lo primero que se ejecuta son las tareas sincronas, mientras que las tareas asincronas y las funciones de temporizador(setTimeout)se ponen en la cola de tareas y se ejecutan en orden despues de las sincronas esto es como javascript de manera eficiente maneja sus operaciones sin bloquear las ejecuciones.
 */
